e4806dc8ad01bea2967d1b9fd203bd9a178aa0a4
llz <luolongzhi@infomedia.com.cn>
2019-04-25 21:35:46 +0800

    ok

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,17 @@ export default class MyDocument extends Document {
         <Head>
           <title>管理后台</title>
           <script src="/static/download.js"> </script>
-          <script src="/static/baidu.js"> </script>
+
+          <script>
+          var _hmt = _hmt || [];
+          (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?1ea8dcdd631698c52efc4e097b3a78da";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+          })();
+          </script>
+
           {styleTags}
         </Head>
         <body>
