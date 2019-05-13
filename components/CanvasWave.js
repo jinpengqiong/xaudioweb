e42632a4da2a5fca54cc1d8513a0d4ddf9c358b6
llz <luolongzhi@infomedia.com.cn>
2019-05-13 23:55:49 +0800

    add CanvasWave

---
+++ components/CanvasWave.js
@@ -0,0 +1,103 @@
+import * as util from 'util';
+import { EventEmitter } from  'events';
+import * as _ from 'lodash';
+
+
+class CanvasWave extends EventEmitter {
+  defaultParams = {
+    container: null,
+
+    wRatio: 0.7,
+    hRatio: 0.4,
+
+    backgroundColor: 'black',
+    waveColor: 'green',
+    progressColor: 'red',
+    cursorColor: '#333',
+    cursorWidth: 1,
+  }
+
+  constructor(params) {
+    super();
+
+    this.params = _.extend({}, this.defaultParams, params);
+
+    //init canvas container and elemnt and context
+    this.container = this.params.container;
+    this.canvasElement = document.getElementById(this.container);
+    this.canvasContext = this.canvasElement.getContext("2d");
+
+    //canvas wave height and width
+    this.innerHeight = window.innerHeight;
+    this.innerWidth = window.innerWidth;
+    this.hRatio = this.params.hRatio;
+    this.wRatio = this.params.wRatio;
+    this.height = this.innerHeight * this.hRatio;
+    this.width = this.innerWidth * this.wRatio;
+    this.canvasElement.width = this.width;
+    this.canvasElement.height = this.height;
+
+    //set each color
+    this.setBackgroundColor(this.params.backgroundColor);
+    this.setWaveColor(this.params.waveColor);
+    this.setProgressColor(this.params.progressColor);
+    this.setCursorColor(this.params.cursorColor);
+
+
+    console.log("current params: ", this.params);
+  }
+
+  static create(params) {
+    const canvasWave = new CanvasWave(params);
+    return canvasWave.init();
+  }
+
+  init() {
+    return this;
+  }
+
+  load() {
+    this.drawBackground();
+    this.drawWave();
+  }
+
+  drawBackground() {
+    this.canvasContext.fillRect(0, 0, this.width, this.height);
+    this.canvasContext.strokeStyle = this.backgroundColor;
+    this.canvasContext.stroke();
+  }
+
+  drawWave() {
+    this.canvasContext.fillRect(20,20,5000,100);
+    this.canvasContext.stroke();
+
+    this.canvasContext.beginPath();
+    this.canvasContext.strokeStyle="red";
+    this.canvasContext.moveTo(20,20);
+    this.canvasContext.lineTo(20,100);
+    this.canvasContext.lineTo(70,100);
+    this.canvasContext.stroke();
+  }
+
+  setBackgroundColor(color) {
+    this.params.backgroundColor = color;
+    //util.style(this.container, { background: this.params.backgroundColor });
+  }
+
+  setWaveColor(color) {
+  }
+
+  setProgressColor(color) {
+  }
+
+  setCursorColor(color) {
+  }
+
+
+}
+
+export default CanvasWave;
+
+
+
+
--- containers/EditPage.js
+++ containers/EditPage.js
@@ -13,6 +13,8 @@ import {CommonHeader, CommonFooter, CommonNoteTip} from './CommonHeaderFooter';
 import Navigation from './Navigation';
 import langmap from '../config/langmap';
 
+import CanvasWave from '../components/CanvasWave';
+
 import { fabric } from 'fabric';
 
 const {Header, Footer, Sider, Content} = Layout;
@@ -25,125 +27,22 @@ const BG_COLOR = "#252729";
 const WAVE_ID = "canvas__wave";
 const WAVE_COLOR = "black";
 
-const getBgContext = () => {
-  let c = document.getElementById(BG_ID);
-  let ctx = c.getContext("2d");
-
-  return ctx;
-}
-
 const CanvasBg = () => {
   //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: '100%'}}></canvas>);
   //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: '1000'}}></canvas>);
 
   //return (<canvas id={BG_ID} width='1000' height='500' style={{backgroundColor: {BG_COLOR}}}></canvas>);
   //return (<canvas id={BG_ID} width='1000' height='500'></canvas>);
-  return (<canvas id={BG_ID} width='2000' height='2000' ></canvas>);
+  //return (<canvas id={BG_ID} width='1000'></canvas>);
+  return (<canvas id={BG_ID} ></canvas>);
   //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: "100%", height: 328}}></canvas>);
   //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: '1000'}}></canvas>);
 }
 
+const TT4 = () => {
+  const canvasWave = CanvasWave.create({container: BG_ID, hRatio: 0.2});
 
-const CanvasWave = () => {
-  //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: '100%'}}></canvas>);
-  //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: '1000'}}></canvas>);
-
-  //return (<canvas id={BG_ID} width='1000' height='500' style={{backgroundColor: {BG_COLOR}}}></canvas>);
-  //return (<canvas id={BG_ID} width='1000' height='500'></canvas>);
-  return (<canvas id={WAVE_ID} ></canvas>);
-  //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: "100%", height: 328}}></canvas>);
-  //return (<canvas id={BG_ID} style={{backgroundColor: {BG_COLOR},  width: '1000'}}></canvas>);
-}
-
-
-const TT = () => {
-  var canvas = new fabric.Canvas(BG_ID);
-  //var canvas = new fabric.StaticCanvas(BG_ID);
-
-  var rect = new fabric.Rect({
-    top : 0,
-    left : 0,
-    width : 1000,// window.innerWidth,
-    height : 500,//window.innerHeight,
-    fill : 'red',
-  });
-
-  var line = new fabric.Line([0, 0, 200, 200], {
-    left: 0,
-    top: 0,
-    stroke: 'red',
-  });
-
-  var line1 = new fabric.Line([10, 0, 200, 200], {
-    left: 10,
-    top: 20,
-    stroke: 'red',
-  });
-
-
-  canvas.add(rect);
-  canvas.add(line);
-  canvas.add(line1);
-
-}
-
-const TT1 = () => {
-  console.log("11111111", window.innerHeight);
-  console.log("2222222222", window.innerWidth);
-  let c=document.getElementById(BG_ID);
-  let ctx=c.getContext("2d");
-
-  ctx.fillRect(0,0,2000,2000);
-  ctx.strokeStyle="grey";
-  ctx.stroke();
-
-  //ctx.beginPath();
-  //ctx.strokeStyle="red";
-  //ctx.moveTo(20,20);
-  //ctx.lineTo(20,100);
-  //ctx.lineTo(70,100);
-  //ctx.stroke();
-
-
-
-
-}
-
-
-const TT3 = () => {
-  console.log("11111111", window.innerHeight);
-  console.log("2222222222", window.innerWidth);
-  let c=document.getElementById(WAVE_ID);
-  let ctx=c.getContext("2d");
-
-  ctx.fillRect(20,20,5000,100);
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.strokeStyle="red";
-  ctx.moveTo(20,20);
-  ctx.lineTo(20,100);
-  ctx.lineTo(70,100);
-  ctx.stroke();
-
-
-
-
-}
-
-
-
-const TT2 = () =>{
-  var wavesurfer = WaveSurfer.create({
-        container: '#waveform',
-        waveColor: 'violet',
-        progressColor: 'purple'
-  });
-
-  wavesurfer.load('/static/cc.mp3');
-  //wavesurfer.on('ready', function() {
-    //wavesurfer.play();
-  //})
+  canvasWave.load();
 }
 
 class EditPage extends React.Component {
@@ -155,12 +54,8 @@ class EditPage extends React.Component {
     const {navigation} = this.props.store;
     navigation.setNav('audio_edit');
 
-    let ctx = getBgContext();
 
-    //TT();
-    TT1();
-
-    TT3();
+    TT4();
   }
 
   componentWillUnmount() {}
@@ -185,7 +80,6 @@ class EditPage extends React.Component {
                 <Row>
                   <Col span={20} offset={0}>
                   <CanvasBg/>
-                  <CanvasWave/>
                   </Col>
                 </Row>
               </WrapperRelative>
